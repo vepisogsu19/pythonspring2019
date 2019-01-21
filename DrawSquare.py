0c2b216d944a9e756b292abad73b5e672d207388
vepisogsu19 <46854569+vepisogsu19@users.noreply.github.com>
2019-01-20 18:18:54 -0800

    Rename DrawingSquare.py to DrawSquare.py
    Program to draw a Square

---
+++ DrawSquare.py
@@ -0,0 +1,17 @@
+# Program to draw Square
+
+import turtle
+
+turtle.forward(120)  # moves forward in the direction its facing by 50
+turtle.right(90)    # makes it turn clock wise by making its turn by 90  degrees
+
+turtle.forward(120)
+turtle.right(90)
+
+turtle.forward(120)
+turtle.right(90)
+
+turtle.forward(120)
+turtle.right(90)
+
+turtle.done()
